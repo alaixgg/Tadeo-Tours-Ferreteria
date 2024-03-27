e3d318bdab1cb4a7c3f23e890cda305e313cf0f2
alaixgg <alaix94@gmail.com>
2024-03-27 14:01:25 -0500

    Constraints, y creacion de tablas , falta detalles pedidos, inventario y prducto

--- ferreteria.sql
+++ ferreteria.sql
@@ -4,6 +4,7 @@ create table cargo(
     salario number not null,
     constraint pk_id_cargo primary key (id_cargo)
 );
+ 
 create table empleado(
 	id_empleado number,
     nombre_empleado nvarchar2(15) not null,
@@ -50,11 +51,44 @@ create table detalle_pedido(
     constraint fk_id_producto foreign key (id_producto) references producto (id_producto),
 		constraint pk_id_pedido primary key (id_pedido)
 )
-)
+create table pago(
+  id_pago number,
+  valor_pago number, 
+  fecha_transacion_pago datetime,
+  forma_pago nvarchar2(30),
+  id_cliente number,
+  id_pedido number,
+  constraint pk_id_cargo primary key (id_pago)
+  constraint fk_id_cliente foreign key references cliente(id_cliente)
+  constraint fk_id_pedido foreign key references pedido (id_pedido)
+);
+create table proveedor(
+  id_proveedor number, 
+  nombre_proveedor nvarchar2(50),
+  correo_proveedor nvarchar2(50),
+  direccion_proveedor nvarchar2(100),
+  telefono_proveedor nvarchar2(10),
+  cuenta_bancaria_proveedor nvarchar2(70),
+  constraint pk_id_proveedor primary key (id_proveedor)
+);
+create table factura(
+  id_factura number, 
+  id_pedido number, 
+  fecha_factura datetime,
+  id_pago number,
+  constraint pk_factura primary key (id_factura)
+  constraint fk_id_pago foreign key (id_pago) references pago (id_pago)
+);
+
 create table producto(
-    id_producto number,
-    nombre_producto nvarchar(40),
- 
- 
- 
- ]
+  id_producto number,
+  nombre_producto nvarchar2(100),
+  id_proveedor number,
+  almacenamiento_producto nvarchar2(30),
+  precio number,
+  marca nvarchar2(20),
+  constraint pk_producto primary key (id_producto)
+  constraint fk_proveedor foreign key (id_proveedor) references proveedor (id_proveedor)
+);
+
+
