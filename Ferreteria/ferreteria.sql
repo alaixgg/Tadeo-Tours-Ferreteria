eeccc0dc7dff666e76ac17b61ae2075af6342061
juliethnana <165108919+juliethnana@users.noreply.github.com>
2024-04-02 21:29:55 -0500

    Update ferreteria.sql

--- Ferreteria/ferreteria.sql
+++ Ferreteria/ferreteria.sql
@@ -9,7 +9,7 @@ create table empleado(
     cargo nvarchar2(30) not null,
     salario number not null,
     constraint pk_id_empleado primary key (id_empleado)
-)
+);
 create table venta(
     id_venta number,
     fecha_venta date not null,
@@ -19,7 +19,7 @@ create table venta(
 		constraint fk_id_cliente foreign key (id_cliente) references cliente (id_cliente),
 		constraint fk_id_pago foreign key (id_pago) references pago (id_pago),
     constraint pk_id_venta primary key (id_venta) 
-)
+);
 create table cliente(
     id_cliente number,
     nombre_cliente nvarchar2(15) not null,
@@ -28,7 +28,7 @@ create table cliente(
     direccion_cliente nvarchar(100) not null,
     correo_cliente nvarchar(50),
     constraint pk_id_cliente primary key (id_cliente)
-)
+);
 create table pedido(
     id_pedido number,
     fecha_entrega date not null,  
@@ -36,7 +36,7 @@ create table pedido(
     estado_pedido boolean,
 		constraint fk_id_venta foreign key (id_venta) references venta (id_venta),
     constraint pk_id_pedido primary key (id_pedido)
-)
+);
 create table detalle_pedido(
     id_detalle_pedido number, 
     cantidad_de_producto number,
@@ -45,7 +45,7 @@ create table detalle_pedido(
 		constraint fk_id_pedido foreign key (id_pedido) references pedido (id_pedido),
     constraint fk_id_producto foreign key (id_producto) references producto (id_producto),
 		constraint pk_id_pedido primary key (id_pedido)
-)
+);
 create table pago(
   id_pago number,
   valor_pago number, 
