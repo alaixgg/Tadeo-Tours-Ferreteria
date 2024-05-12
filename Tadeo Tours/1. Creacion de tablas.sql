f8971190070da53304233e035123a1a98bb01263
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-05-11 20:43:10 -0500

    Update and rename tadeo_tours.sql to 1. Creacion de tablas.sql

--- Tadeo Tours/1. Creacion de tablas.sql
+++ Tadeo Tours/1. Creacion de tablas.sql
@@ -1,35 +1,29 @@
-CREATE TABLE ciudad(
+CREATE TABLE CIUDAD(
     id_ciudad number,
-    ciudad_origen nvarchar2(100) not null, 
-    ciudad_destino nvarchar2(100) not null, 
-    ciudad_adicional nvarchar2(50),
+    nombre_ciudad nvarchar2(100) not null, 
     constraint pk_id_ciudad primary key (id_ciudad)
 );
 
-CREATE TABLE hotel(
+CREATE TABLE HOTEL(
     id_hotel number,
-    id_reserva_hotel number,
     nombre_hotel nvarchar2(30) not null,
-    ciudad nvarchar2(50) not null,
+    id_ciudad number,
     categoria nvarchar2(10) not null,
     constraint pk_id_hotel primary key (id_hotel)
 );
 
 CREATE TABLE SOLICITUD_RESERVA(
     id_solicitud number, 
-    fecha_solicitud timestamp not null,
+    id_fecha number,
     id_cliente number, 
     id_ciudad number,
-    id_reserva number, 
-    id_estado number,
+    id_estado number UNIQUE,
     constraint pk_id_solicitud primary key (id_solicitud)
 );
 
-CREATE TABLE estado(
+CREATE TABLE ESTADO(
     id_estado number, 
     fecha_aprobacion timestamp, 
-    id_pago number,
-    id_reserva number,
     comprobacion nvarchar2(10),
     constraint pk_id_estado primary key (id_estado)
 );
@@ -42,55 +36,56 @@ CREATE TABLE aerolinea(
 
 CREATE TABLE pago(
     id_pago number, 
-    id_reserva number,
     total number,
     saldo number, 
     metodo_pago nvarchar2(30),
     fecha_pago timestamp not null,
-    aprobacion char(1) not null,
+    id_estado number,
     constraint pk_id_pago primary key (id_pago)
 );
 
 CREATE TABLE VUELO(
     id_vuelo number,
-    id_aerolinea number,
-    id_reserva_avion number,
+    id_aerolinea number UNIQUE,
     numero_vuelo number,
+    id_fecha number,
+    id_ciudad_origen number,
+    id_ciudad_destino,
     constraint pk_id_vuelo primary key (id_vuelo)
 );
 
 CREATE TABLE RESERVA_AVION(
     id_reserva_avion number,
-    fecha_ida timestamp not null, 
-    fecha_regreso timestamp not null, 
-    origen nvarchar2(20) not null, 
-    destino nvarchar2(20) not null,
+    id_vuelo number,
+    id_viajero number,
     clase nvarchar2(10) not null, 
     constraint pk_id_reserva_avion primary key (id_reserva_avion)
 );
 
 CREATE TABLE RESERVA_HOTEL(
     id_reserva_hotel number,
-    fecha_entrada timestamp not null,
-    fecha_salida timestamp not null, 
+    id_fecha number,
+    id_hotel number,
     tipo_acomodacion nvarchar2(10) not null,
     constraint pk_id_reserva_hotel primary key (id_reserva_hotel)
 );
 
-CREATE TABLE reserva(
+CREATE TABLE RESERVA(
     id_reserva number,
-    id_vuelo number, 
-    id_reserva_hotel number,
-    id_pago number,
+    id_reserva_avion number UNIQUE, 
+    id_reserva_hotel number UNIQUE,
+    id_pago number UNIQUE,
     id_cliente number,
     Numero_Viajeros number, 
-    id_estado number, 
+    id_estado number UNIQUE, 
+    id_solicitud number UNIQUE,
     constraint pk_id_reserva primary key (id_reserva)
 );
 
 CREATE TABLE cliente(
     id_cliente number,
-    nombre_cliente nvarchar2(100) not null, 
+    nombre_cliente nvarchar2(25) not null, 
+    apellido_cliente nvarchar2(25) not null,
     telefono_casa nvarchar2(10) not null,
     telefono_trabajo nvarchar2(10) not null,
     fax nvarchar2(10) not null,
@@ -102,13 +97,14 @@ CREATE TABLE cliente(
 CREATE TABLE VIAJERO(
     id_viajero number,
     id_cliente number, 
+    nombre_viajero nvarchar2(10) not null,
     edad number,
     constraint pk_id_viajero primary key (id_viajero)
 );
 
-CREATE TABLE fecha_reserva(
-    id_fecha_reserva number,
-    fecha_inicio_reserva timestamp not null, 
-    fecha_fin_reserva timestamp not null,
+CREATE TABLE FECHA(
+    id_fecha number,
+    fecha_inicio timestamp not null, 
+    fecha_fin timestamp not null,
     constraint pk_id_fecha_reserva primary key (id_fecha_reserva)
 );
